bb48a0fd77319de0731678334e3c7731606a26b8
ITISK0 <138399233+ITISK0@users.noreply.github.com>
2023-12-03 01:00:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let isProxy=false;
-object.onload = function(){
+document.addEventListener("DOMContentLoaded", function() {
 	if(window.location.hostname.split(".")[0].startsWith("proxy")){isProxy=true;
 		document.getElementById('replace-me').innerText = "Accessed Via Proxy";
 	}
-};
+});
