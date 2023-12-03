b6e52abf333b9ec875426d3290e0f7a0cbc4e33d
ITISK0 <138399233+ITISK0@users.noreply.github.com>
2023-12-03 02:59:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let isProxy=false;
+window.addEventListener("message", receiveMessage, false);
 function receiveMessage(event) {
 	var origin = event.origin || event.originalEvent.origin; 
 	if (origin === "https://proxy183743.herratech.net"){isProxy=true;
