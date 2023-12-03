d4a9ccf365a7c302a40319c1a448bd267cd01c80
ITISK0 <138399233+ITISK0@users.noreply.github.com>
2023-12-03 01:17:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let isProxy=false;
-document.addEventListener("DOMContentLoaded", function() {
+window.onload=function() {
 	if(document.title.startsWith("**")){isProxy=true;
 		document.getElementById('replace-me').innerText="Accessed Via Proxy";
 		document.title="Herratech - Home"
 	}
-});
+};
